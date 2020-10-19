16afd5970a7f7c4bffb31fd1a6fbbb4245db06ad
EugenVLS <evgeniy1995vlasov@gmail.com>
2020-10-20 00:21:36 +0500

    edit product

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
-});
+sequelize
+    .sync()
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
